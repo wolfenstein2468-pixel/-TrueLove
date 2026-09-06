0135e563d210f8763963bebde7ac9dd43feef1f9
wolfenstein2468-pixel <wolfenstein2468@gmail.com>
2026-09-06 12:02:34 +0500

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import './styles.css';
+import { VaultModule } from './modules/vault.ts';
+import data from './data.json';
+
+// Забираем секретный код из json
+const secretCode = data.vault.secretCode as [number, number, number, number];
+
+new VaultModule(secretCode, () => {
+  const app = document.getElementById('app');
+  if (app) {
+    app.innerHTML = `<div style="text-align: center; font-size: 1.5rem; margin-top: 40vh; color: #3fb950;">Сейф успешно открыт! Переход к следующей сцене...</div>`;
+  }
+});
